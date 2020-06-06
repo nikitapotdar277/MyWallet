9dccc41224cfcee5ebbc13fbf9915c22108860da
nikitapotdar277 <60482861+nikitapotdar277@users.noreply.github.com>
2020-06-06 17:41:43 +0530

    Update allexp1.js

--- allexp1.js
+++ allexp1.js
@@ -10,15 +10,22 @@ window.onload = function() {
 	function displayList() {
 			allexpenses = JSON.parse(localStorage.getItem("expenses"));
 			// console.log(allexpenses);
+			var sum = 0;
+			for (var i = 0; i < allexpenses.length; i++) {
+				sum += parseInt(allexpenses[i].amount);
+			}
 			$.each(allexpenses, function(key,value) {
 			$('tbody').append ( `<tr>
 					<td class = "Date">${value.date}</td>
-					// <td class = "Category">${value.category}</td>
-					// <td class = "Amount">${value.amount}</td>
+					<td class = "Category">${value.category}</td>
+					<td class = "Amount">${value.amount}</td>
 					</tr>`
 					);
 			 })
-			
+			$('tbody').append(`<tr>
+			<td colspan = "2"> <b>TOTAL<b> </td>
+			<th>${sum}</td>
+			</tr>`);
 		}
 	displayList();
 
